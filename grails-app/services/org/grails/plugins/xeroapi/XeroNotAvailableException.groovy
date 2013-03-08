225fa6da46484d5a3d2f0028fa2f92f38c0c4d7f
Stephanie Sherriff <stephanie@trexart.com.au>
2013-03-08 20:30:29 +1100

    Updated XeroOrganisationService Cleaned up some code not needed Updated to read XML as JSON dates weren't working Got date values correctly setting

---
+++ grails-app/services/org/grails/plugins/xeroapi/XeroNotAvailableException.groovy
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.grails.plugins.xeroapi
+
+/**
+ *
+ * @author steph
+ */
+class XeroNotAvailableException extends XeroException {
+
+	XeroNotAvailableException() { }
+	
+}
+
--- grails-app/services/org/grails/plugins/xeroapi/XeroOrganisationService.groovy
+++ grails-app/services/org/grails/plugins/xeroapi/XeroOrganisationService.groovy
@@ -5,7 +5,6 @@
 
 package org.grails.plugins.xeroapi
 
-import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 import grails.converters.*
 
 import uk.co.desirableobjects.oauth.scribe.OauthService
@@ -18,7 +17,6 @@ import org.scribe.model.Token
  */
 class XeroOrganisationService {
     final static String API_URL = "https://api.xero.com/api.xro/2.0/Organisation"
-    final static String REQUEST = "Organisation"
     
     boolean transactional = false
 
@@ -30,34 +28,39 @@ class XeroOrganisationService {
     }
 	
     def get() throws XeroUnauthorizedException, XeroException {
-        String key = CH.config.xero.key.toString()
-        String secret = CH.config.xero.secret.toString()
         def org = new XeroOrganisation()
+        def dateFormat = new java.text.SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" )
+        def dateFormatUTC = new java.text.SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss.sss" )
         
-        def resp = oauthService.getXeroResource(oauthToken, API_URL, null, ['Content-Type':'application/json', 'Accept':'application/json'])
+        //def resp = oauthService.getXeroResource(oauthToken, API_URL, null, ['Content-Type':'application/json', 'Accept':'application/json'])
+        def resp = oauthService.getXeroResource(oauthToken, API_URL)
         //log.debug(resp.getCode())
 
         if( resp.getCode() == 200 ) {
             //log.debug(resp.getBody())
 
-            def json = JSON.parse(resp.getBody())
+            def json = XML.parse(resp.getBody())
+            //org.apiKey = json.Organisations[0].APIKey
+            org.apiKey = json.Organisations.Organisation.APIKey
+            org.name = json.Organisations.Organisation.Name
+            org.legalName = json.Organisations.Organisation.LegalName
+            org.paysTax = json.Organisations.Organisation.PaysTax
+            org.taxNumber = json.Organisations.Organisation.TaxNumber
+            org.registrationNumber = json.Organisations.Organisation.RegistrationNumber
+            org.version = json.Organisations.Organisation.Version
+            org.organisationType = json.Organisations.Organisation.OrganisationType
+            org.status = json.Organisations.Organisation.OrganisationStatus
+            org.financialYearEndDay = ((json.Organisations.Organisation.FinancialYearEndDay as String) as Integer)
+            org.financialYearEndMonth = ((json.Organisations.Organisation.FinancialYearEndMonth as String) as Integer)
+            org.baseCurrency = json.Organisations.Organisation.BaseCurrency
+            org.countryCode = json.Organisations.Organisation.CountryCode
+            org.demoCompany = json.Organisations.Organisation.IsDemoCompany
 
-            org.apiKey = json.Organisations[0].APIKey
-            org.name = json.Organisations[0].Name
-            org.legalName = json.Organisations[0].LegalName
-            org.paysTax = json.Organisations[0].PaysTax
-            org.taxNumber = json.Organisations[0].TaxNumber
-            org.registrationNumber = json.Organisations[0].RegistrationNumber
-            org.version = json.Organisations[0].Version
-            org.organisationType = json.Organisations[0].OrganisationType
-            org.status = json.Organisations[0].OrganisationStatus
-            org.financialYearEndDay = json.Organisations[0].FinancialYearEndDay
-            org.financialYearEndMonth = json.Organisations[0].FinancialYearEndMonth
-            org.baseCurrency = json.Organisations[0].BaseCurrency
-            org.countryCode = json.Organisations[0].CountryCode
-            org.demoCompany = json.Organisations[0].IsDemoCompany
-            /*org.periodLockDate = json.Organisations[0].PeriodLockDate
-            org.createdDateUTC = json.Organisations[0].CreatedDateUTC*/
+            String dateString = json.Organisations.Organisation.PeriodLockDate
+            org.periodLockDate = dateFormat.parse(dateString)
+
+            dateString = json.Organisations.Organisation.CreatedDateUTC
+            org.createdDateUTC = dateFormatUTC.parse(dateString)
         } else {
             log.debug(resp.getBody())
 
@@ -68,14 +71,9 @@ class XeroOrganisationService {
                 case 401:
                     throw new XeroUnauthorizedException()
                     break
-                /*case 404:
-                    // throw new XeroNotFoundException
-                    //break
-                case 501:
-                    // throw new XeroNotImplementedException
-                    
                 case 503:
-                    // throw new XeroNotAvailableException*/
+                    throw new XeroNotAvailableException()
+                    break
                 default:
                     throw new XeroException()
                 
