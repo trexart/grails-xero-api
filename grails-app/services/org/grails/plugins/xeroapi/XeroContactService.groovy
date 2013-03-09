0bbdebf72bf4f5773e2283f58cf7d5f4828d5546
Stephanie Sherriff <stephanie@trexart.com.au>
2013-03-09 20:24:02 +1100

    Refactored a bit so I don't repeat as much code

--- grails-app/services/org/grails/plugins/xeroapi/XeroContactService.groovy
+++ grails-app/services/org/grails/plugins/xeroapi/XeroContactService.groovy
@@ -88,7 +88,7 @@ class XeroContactService {
         return contact
     }
 
-    def getAll(Date modifiedSince = null) throws XeroUnauthorizedException, XeroException {
+    def findAll(Date modifiedSince = null) throws XeroUnauthorizedException, XeroException {
         def contacts = []
 
         def headers = ['Content-Type':'application/json', 'Accept':'application/json']
@@ -101,7 +101,58 @@ class XeroContactService {
             headers["If-Modified-Since"] = outFormat.format(modifiedSince)
         }
 
-        def resp = oauthService.getXeroResource(oauthToken, API_URL, null, headers)
+        return getListResult(API_URL, headers)
+    }
+
+    def methodMissing(String name, args) throws XeroUnauthorizedException, XeroException, MissingMethodException {
+        log.debug("methodMissing name: ${name}")
+        log.debug("methodMissing args: ${args}")
+
+        def xc = new XeroContact()
+        String url = API_URL
+
+        if(name.startsWith("findAllBy")) {
+
+            String instructions = name.replaceAll("findAllBy", "")
+
+            Date modifiedSince = null
+
+            if(xc.metaClass.hasProperty(xc, GrailsNameUtils.getPropertyName(instructions))) {
+                String where = 'IsCustomer==' + args[0]
+                url += "?where=" + where.encodeAsURL()
+
+                if(args.size() > 1 && args[1] instanceof Date) {
+                    modifiedSince = args[1]
+                }
+            } else {
+                throw new MissingMethodException(name, this.class, args)
+            }
+
+            log.debug("url: ${url}")
+
+            def headers = ['Content-Type':'application/json', 'Accept':'application/json']
+
+            if(modifiedSince != null) {
+                def outFormat = new java.text.SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" )
+                outFormat.timeZone = java.util.TimeZone.getTimeZone( 'GMT' )
+                log.debug 'modified date in local time: ' + modifiedSince.format("yyyy-MM-dd'T'HH:mm:ss")
+                log.debug 'modified date in GMT: ' + outFormat.format(modifiedSince)
+                headers["If-Modified-Since"] = outFormat.format(modifiedSince)
+            }
+
+            return getListResult(url, headers)
+
+            // this caches the method
+            //this.metaClass."$name" = {-> result }
+        } else {
+            throw new MissingMethodException(name, this.class, args)       
+        }
+    }
+
+    private def getListResult(String url, def headers) throws XeroUnauthorizedException, XeroException {
+        def contacts = []
+
+        def resp = oauthService.getXeroResource(oauthToken, url, null, headers)
         //log.debug(resp.getCode())
 
         if( resp.getCode() == 200 ) {
@@ -136,85 +187,5 @@ class XeroContactService {
         }
         
         return contacts
-    }
-
-    def methodMissing(String name, args) throws XeroUnauthorizedException, XeroException, MissingMethodException {
-        log.debug("methodMissing name: ${name}")
-        log.debug("methodMissing args: ${args}")
-
-        if(name.startsWith("findAllBy")) {
-
-            String instructions = name.replaceAll("findAllBy", "")
-            def xc = new XeroContact()
-            def contacts = []
-
-            String url = API_URL
-
-            Date modifiedSince = null
-
-            if(xc.metaClass.hasProperty(xc, GrailsNameUtils.getPropertyName(instructions))) {
-                String where = 'IsCustomer==' + args[0]
-                url += "?where=" + where.encodeAsURL()
-
-                if(args.size() > 1 && args[1] instanceof Date) {
-                    modifiedSince = args[1]
-                }
-            } else {
-                throw new MissingMethodException(name, this.class, args)
-            }
-
-            log.debug("url: ${url}")
-
-            def headers = ['Content-Type':'application/json', 'Accept':'application/json']
-
-            if(modifiedSince != null) {
-                def outFormat = new java.text.SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" )
-                outFormat.timeZone = java.util.TimeZone.getTimeZone( 'GMT' )
-                log.debug 'modified date in local time: ' + modifiedSince.format("yyyy-MM-dd'T'HH:mm:ss")
-                log.debug 'modified date in GMT: ' + outFormat.format(modifiedSince)
-                headers["If-Modified-Since"] = outFormat.format(modifiedSince)
-            }
-
-            def resp = oauthService.getXeroResource(oauthToken, url, null, headers)
-            //log.debug(resp.getCode())
-
-            if( resp.getCode() == 200 ) {
-                //log.debug(resp.getBody())
-
-                def json = JSON.parse(resp.getBody())
-
-                def contact
-
-                json.Contacts.each {  // iterate over JSON 'status' object in the response:
-                    //println(it)
-                    contact = fillContact(it)
-                    contacts.add(contact)
-                }
-            } else {
-                log.debug(resp.getBody())
-
-                switch (resp.getCode()) {
-                    case 400: 
-                        throw new XeroBadRequestException()
-                        break
-                    case 401:
-                        throw new XeroUnauthorizedException()
-                        break
-                    case 503:
-                        throw new XeroNotAvailableException()
-                        break
-                    default:
-                        throw new XeroException()
-                    
-                }
-            }
-            
-            return contacts
-
-            // this caches the method
-            //this.metaClass."$name" = {-> result }
-        } else {
-            throw new MissingMethodException(name, this.class, args)       
-        }
     }
 }
