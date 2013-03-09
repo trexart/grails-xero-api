d5a9a2622e9a9713e2cbb504c7f49e2b68479394
Stephanie Sherriff <stephanie@trexart.com.au>
2013-03-09 23:43:42 +1100

    Refactored to remove a bunch of duplicate code plus cleaned it up

--- grails-app/services/org/grails/plugins/xeroapi/XeroInvoiceService.groovy
+++ grails-app/services/org/grails/plugins/xeroapi/XeroInvoiceService.groovy
@@ -5,8 +5,8 @@
 
 package org.grails.plugins.xeroapi
 
-import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 import grails.converters.*
+import grails.util.GrailsNameUtils
 
 import uk.co.desirableobjects.oauth.scribe.OauthService
 
@@ -18,7 +18,6 @@ import org.scribe.model.Token
  */
 class XeroInvoiceService {
     final static String API_URL = "https://api.xero.com/api.xro/2.0/Invoices"
-    final static String REQUEST = "Invoices"
     
     boolean transactional = false
     
@@ -29,60 +28,12 @@ class XeroInvoiceService {
         oauthToken = token
     }
 
-    def getAll() throws XeroUnauthorizedException, XeroException {
-        String key = CH.config.xero.key.toString()
-        String secret = CH.config.xero.secret.toString()
-        def invoices = []
+    def getAll(Date modifiedSince = null) throws XeroUnauthorizedException, XeroException {
 
-        def resp = oauthService.getXeroResource(oauthToken, API_URL, null, ['Content-Type':'application/json', 'Accept':'application/json'])
-        //log.debug(resp.getCode())
-
-        if( resp.getCode() == 200 ) {
-            //log.debug(resp.getBody())
-
-            def json = JSON.parse(resp.getBody())
-
-            def invoice
-
-            json.Invoices.each {  // iterate over JSON 'status' object in the response:
-                //println(it)
-                invoice = new XeroInvoice()
-                invoice.id = it.InvoiceID
-                invoice.invoiceNumber = it.InvoiceNumber
-
-                invoices.add(invoice)
-            }
-        } else {
-            log.debug(resp.getBody())
-
-            switch (resp.getCode()) {
-                case 400: 
-                    throw new XeroBadRequestException()
-                    break
-                case 401:
-                    throw new XeroUnauthorizedException()
-                    break
-                /*case 404:
-                    // throw new XeroNotFoundException
-                    //break
-                case 501:
-                    // throw new XeroNotImplementedException
-                    
-                case 503:
-                    // throw new XeroNotAvailableException*/
-                default:
-                    throw new XeroException()
-                
-            }
-        }
-        
-        return invoices
+        return getListResult(API_URL, modifiedSince)
     }
 
     def getAllByType(String type, String status = null, Date modifiedSince = null) throws XeroException {
-        String key = CH.config.xero.key.toString()
-        String secret = CH.config.xero.secret.toString()
-        def invoices = []
         
         String where = 'Type=="' + type + '"'
 
@@ -93,17 +44,53 @@ class XeroInvoiceService {
         String url = API_URL + "?where=" + where.encodeAsURL()
         log.debug 'url: ' + url
 
+        return getListResult(url, modifiedSince)
+    }
 
+    def getAllPaidSince(Date paidSince, Date modifiedSince = null) throws XeroException {
 
-        /*if(modifiedSince != null) {
+        String where = 'FullyPaidOnDate >= DateTime(' + (paidSince.year+1900) + ', ' + (paidSince.month+1) + ', ' + paidSince.date + ')'
+        log.debug 'where: ' + where
+
+        String url = API_URL + "?where=" + where.encodeAsURL()
+        log.debug 'url: ' + url
+
+        return getListResult(url, modifiedSince)
+    }
+
+    private def fillInvoice(def it) {
+        def invoice = new XeroInvoice()
+        invoice.id = it.InvoiceID
+        invoice.invoiceNumber = it.InvoiceNumber
+        invoice.status = it.Status
+
+        def contact = new XeroContact()
+        contact.id = it.Contact.ContactID
+        contact.name = it.Contact.Name
+        contact.email = it.Contact.EmailAddress
+        contact.status = it.Contact.ContactStatus
+        //contact.supplier = it.Contact.IsSupplier
+        //contact.customer = it.Contact.IsCustomer
+
+        invoice.contact = contact
+
+        return invoice
+    }
+
+    private def getListResult(String url, Date modifiedSince = null) throws XeroUnauthorizedException, XeroException {
+        def invoices = []
+
+        def headers = ['Content-Type':'application/json', 'Accept':'application/json']
+
+        if(modifiedSince != null) {
             def outFormat = new java.text.SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" )
             outFormat.timeZone = java.util.TimeZone.getTimeZone( 'GMT' )
             log.debug 'modified date in local time: ' + modifiedSince.format("yyyy-MM-dd'T'HH:mm:ss")
             log.debug 'modified date in GMT: ' + outFormat.format(modifiedSince)
-            restClient.headers["If-Modified-Since"] = outFormat.format(modifiedSince)
-        }*/
+            headers["If-Modified-Since"] = outFormat.format(modifiedSince)
+        }
 
-        def resp = oauthService.getXeroResource(oauthToken, url, null, ['Content-Type':'application/json', 'Accept':'application/json'])
+        def resp = oauthService.getXeroResource(oauthToken, url, null, headers)
         //log.debug(resp.getCode())
 
         if( resp.getCode() == 200 ) {
@@ -112,24 +99,10 @@ class XeroInvoiceService {
             def json = JSON.parse(resp.getBody())
 
             def invoice
-            def contact
 
             json.Invoices.each {  // iterate over JSON 'status' object in the response:
                 //println(it)
-                invoice = new XeroInvoice()
-                invoice.id = it.InvoiceID
-                invoice.invoiceNumber = it.InvoiceNumber
-                invoice.status = it.Status
-
-                contact = new XeroContact()
-                contact.id = it.Contact.ContactID
-                contact.name = it.Contact.Name
-                contact.email = it.Contact.EmailAddress
-                contact.status = it.Contact.ContactStatus
-                //contact.supplier = it.Contact.IsSupplier
-                //contact.customer = it.Contact.IsCustomer
-
-                invoice.contact = contact
+                invoice = fillInvoice(it)
 
                 invoices.add(invoice)
             }
@@ -143,82 +116,9 @@ class XeroInvoiceService {
                 case 401:
                     throw new XeroUnauthorizedException()
                     break
-                /*case 404:
-                    // throw new XeroNotFoundException
-                    //break
-                case 501:
-                    // throw new XeroNotImplementedException
-                    
                 case 503:
-                    // throw new XeroNotAvailableException*/
-                default:
-                    throw new XeroException()
-                
-            }
-        }
-        
-        return invoices
-    }
-
-    def getAllPaidSince(Date paidSince) throws XeroException {
-        String key = CH.config.xero.key.toString()
-        String secret = CH.config.xero.secret.toString()
-        def invoices = []
-        //FullyPaidOnDate >= DateTime(2011, 10, 01)
-        String where = 'FullyPaidOnDate >= DateTime(' + (paidSince.year+1900) + ', ' + (paidSince.month+1) + ', ' + paidSince.date + ')'
-        log.debug 'where: ' + where
-
-        String url = API_URL + "?where=" + where.encodeAsURL()
-        log.debug 'url: ' + url
-
-        def resp = oauthService.getXeroResource(oauthToken, url, null, ['Content-Type':'application/json', 'Accept':'application/json'])
-        log.debug(resp.getCode())
-
-        if( resp.getCode() == 200 ) {
-            log.debug(resp.getBody())
-
-            def json = JSON.parse(resp.getBody())
-
-            def invoice
-            def contact
-
-            json.Invoices.each {  // iterate over JSON 'status' object in the response:
-                //println(it)
-                invoice = new XeroInvoice()
-                invoice.id = it.InvoiceID
-                invoice.invoiceNumber = it.InvoiceNumber
-                invoice.status = it.Status
-
-                contact = new XeroContact()
-                contact.id = it.Contact.ContactID
-                contact.name = it.Contact.Name
-                contact.email = it.Contact.EmailAddress
-                contact.status = it.Contact.ContactStatus
-                //contact.supplier = it.Contact.IsSupplier
-                //contact.customer = it.Contact.IsCustomer
-
-                invoice.contact = contact
-
-                invoices.add(invoice)
-            }
-        } else {
-            log.debug(resp.getBody())
-
-            switch (resp.getCode()) {
-                case 400: 
-                    throw new XeroBadRequestException()
+                    throw new XeroNotAvailableException()
                     break
-                case 401:
-                    throw new XeroUnauthorizedException()
-                    break
-                /*case 404:
-                    // throw new XeroNotFoundException
-                    //break
-                case 501:
-                    // throw new XeroNotImplementedException
-                    
-                case 503:
-                    // throw new XeroNotAvailableException*/
                 default:
                     throw new XeroException()
                 
